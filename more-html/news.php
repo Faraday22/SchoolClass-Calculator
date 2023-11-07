1547178a3daedbb3d51b0dbd3689a270a7565cda
Faraday <115096700+Faraday22@users.noreply.github.com>
2023-11-07 20:42:00 +0000

    Getting Ready for next feature

---
+++ more-html/news.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<head>
+  <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
+  
+  
+  
+  <!-- Update when new css file is made -->
+  <link rel="stylesheet" href="../index.css"/>
+  
+  
+  
+  <title>News</title>
+    <!--Top Bar Thingy-->
+    <table class="topbar">
+        <tr>
+          <th><a href="https://faraday22.github.io/SchoolClass-Calculator/more-html/info.html">Information</a></th>
+          <th>|</th>
+          <th><a href="https://faraday22.github.io/SchoolClass-Calculator/">Home</a></th>
+          <th>|</th>
+          <th><a href="https://github.com/Faraday22/SchoolClass-Calculator">Github</a></th>
+      </tr></table>
+      <hr id="textUnderLineThing">
+      <br>
+</head>
+<body>
+    
+    <h2>News</h2>
+    <hr id="textUnderLineThing">
+    <!-- Code for new pharagraph / where info will be listed -->
+    <!-- make have a border that goes into the background in a darker shade too so new css file for this page -->
+
+
+    <!-- at bottom of page area to submit information -->
+    <?php 
+    //
+    // Insert Code for info
+    // Submittion Here
+    //
+    ?>
+
+</body>
+
