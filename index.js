de28c952319c0dffab79a86cb08f78fc3be8c200
Faraday <115096700+Faraday22@users.noreply.github.com>
2023-12-07 18:55:42 +0000

    Trying to fix array numbers being assigned wrong

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import { halfDayPeriods } from "./more-javascript/halfDayScript"; 
 
-var timeLeft;
+var timeLeft = null;
 var startTime = 8.47;
 var date = new Date();
 /*
@@ -67,44 +67,52 @@ function isThird(){
 
 function isFourth(){
  if (currentTime > periods.time[2]){
-    alert("Your in " + periods[4].name);
+    alert("Your in " + periods[3].name);
     timeLeft = periods[3].time - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   } 
 }
 
-function isLunch(){
+function isFifth(){
  if (currentTime > periods[3].time){
-    alert("Your in " + periods[5].name);
+    alert("Your in " + periods[4].name);
     timeLeft = periods[4].time - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   } 
 }
 
-function isSixth(){
- if (currentTime > periods[4].time){
-    alert("Your in " + periods[6].name);
-    timeLeft = periods[5].time - currentTime;
+function isLunch(){
+  if (currentTime > periods[4].time){
+    alert("Your at " + periods[5].name);
+    timeLeft = periods[5] - currentTime;
     alert("There is: " + timeLeft + " time left");
-    return;
-  } 
+  }
 }
 
-function isSeventh(){
+function isSixth(){
  if (currentTime > periods[5].time){
-    alert("Your in " + periods[7].name);
+    alert("Your in " + periods[6].name);
     timeLeft = periods[6].time - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   } 
 }
 
-function isEigth(){
+function isSeventh(){
  if (currentTime > periods[6].time){
-    alert("Your in " + periods[8].name);
+    alert("Your in " + periods[7].name);
     timeLeft = periods[7].time - currentTime;
+    alert("There is: " + timeLeft + " time left");
+    return;
+  } 
+}
+
+function isEigth(){
+ if (currentTime > periods[7].time){
+    alert("Your in " + periods[8].name);
+    timeLeft = periods[8].time - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   } 
@@ -123,6 +131,7 @@ function findPeriod(){
   isSecond();
   isThird();
   isFourth();
+  isFifth();
   isLunch();
   isSixth();
   isSeventh();
