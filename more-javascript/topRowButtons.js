5dd32659913cf260975e2febbdcb8a306ff97a84
Faraday <115096700+Faraday22@users.noreply.github.com>
2023-12-08 17:26:30 +0000

    Fixed Favicon and made links buttons

---
+++ more-javascript/topRowButtons.js
@@ -0,0 +1,27 @@
+/* Plan to use this file for making the top
+*  row not links to the other files but javascript buttons
+*/
+
+function buttonToNews(){
+    location.replace("../more-html/news.html");
+}
+
+function buttonToCredits(){
+    location.replace("../more-html/credits.html");
+}
+
+function buttonToInfo(){
+    location.replace("../more-html/info.html");
+}
+
+function buttonToHome(){
+    location.replace("../index.html");
+}
+
+function buttonToGithub(){
+    window.open("https://github.com/Faraday22/SchoolClass-Calculator", "_blank");
+}
+
+function buttonToCredits(){
+    location.replace("../more-html/credits.html");
+}
