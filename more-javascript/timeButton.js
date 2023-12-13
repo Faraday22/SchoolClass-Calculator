14631f3bd2be5d5fde4737aa156c52a13eb27810
Faraday <115096700+Faraday22@users.noreply.github.com>
2023-12-13 17:21:45 +0000

    Trying to fix button :D

--- more-javascript/timeButton.js
+++ more-javascript/timeButton.js
@@ -12,6 +12,17 @@ var currentTime = date.getHours() + date.getMinutes() / 100;
 //var IsWeekend = date.getDay;
 
 
+var endOfAdvisory = 9.11;
+var endOfSecond = 9.57;
+var endOfThird = 10.48;
+var endOfFourth = 11.39;
+var endOfFifth = 12.30;
+var endOfLunch = 13.05;
+var endOfSixth = 13.56;
+var endOfSeventh = 14.47;
+var endOfEigth = 15.40;
+
+
 var schoolActive = null;
 // is school active / has it started
 if (currentTime <= 8.47){
@@ -23,86 +34,88 @@ if (currentTime <= 8.47){
 } 
 
 function isAdvistory(){
-
-    /* write this so if the current time is
-    *  more than 8 47 and less than 9 11
-    *  etc just use schedule to find out
-    */ 
-    if (currentTime <= [0].time){
-      alert("Your in " + periods[0].name);
-      timeLeft = periods[0].time - currentTime;
+    if (currentTime >= startTime && currentTime > endOfAdvisory){
+      alert("Your in advisory");
+      timeLeft = 9.11 - currentTime;
       alert("There is: " + timeLeft + " time left");
       return;
     }
 }
 
 function isSecond(){
-  if (currentTime >= periods[0].time){
-    alert("Your in " + periods[1].name);
-    timeLeft = periods[1].time - currentTime;
+  if (currentTime > endOfAdvisory && currentTime > endOfSecond){
+    alert("Your in second period");
+    timeLeft = endOfSecond - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   }
 }
 
 function isThird(){
-  if (currentTime >= periods[1].time){
-    alert("Your in " + periods[2].name);
-    timeLeft = periods[2].time - currentTime;
+  if (currentTime > endOfSecond && currentTime > endOfThird){
+    alert("Your in third period");
+    timeLeft = endOfThird - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   }
 }
 
 function isFourth(){
- if (currentTime >= periods.time[2]){
-    alert("Your in " + periods[3].name);
-    timeLeft = periods[3].time - currentTime;
+  if (currentTime > endOfThird && currentTime > endOfFourth){
+    alert("Your in fourth period");
+    timeLeft = endOfFourth - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   } 
 }
 
 function isFifth(){
- if (currentTime >= periods[3].time){
-    alert("Your in " + periods[4].name);
-    timeLeft = periods[4].time - currentTime;
+  if (currentTime > endOfFourth && currentTime > endOfFifth){
+    alert("Your in fifth period");
+    timeLeft = endOfFifth - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   } 
 }
 
 function isLunch(){
-  if (currentTime >= periods[4].time){
-    alert("Your at " + periods[5].name);
-    timeLeft = periods[5] - currentTime;
+  if (currentTime > endOfFifth && currentTime > endOfLunch){
+
+    const easteregg = Math.floor(Math.random() * 100 ) + 1;
+
+    if (easteregg == 52){
+      alert("Your in munchy munch time");
+    } else{
+    alert("Your in  period");
+    }
+    timeLeft = endOfSixth - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   }
 }
 
 function isSixth(){
- if (currentTime >= periods[5].time){
-    alert("Your in " + periods[6].name);
-    timeLeft = periods[6].time - currentTime;
+  if (currentTime > endOfLunch && currentTime > endOfSixth){
+    alert("Your in sixth period");
+    timeLeft = endOfSixth - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   } 
 }
 
 function isSeventh(){
- if (currentTime >= periods[6].time){
-    alert("Your in " + periods[7].name);
-    timeLeft = periods[7].time - currentTime;
+  if (currentTime > endOfSixth && currentTime > endOfSeventh){
+    alert("Your in seventh period");
+    timeLeft = endOfSeventh - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   } 
 }
 
 function isEigth(){
- if (currentTime >= periods[7].time){
-    alert("Your in " + periods[8].name);
-    timeLeft = periods[8].time - currentTime;
+  if (currentTime > endOfSeventh && currentTime > endOfEigth){
+    alert("Your in eight period");
+    timeLeft = endOfEigth - currentTime;
     alert("There is: " + timeLeft + " time left");
     return;
   } 
@@ -130,7 +143,7 @@ function findPeriod(){
 }
 
 
-function schoolTimeCalc() {
+function schoolTimeCalc(){
   if (schoolActive === true){
     findPeriod();
   } else{
