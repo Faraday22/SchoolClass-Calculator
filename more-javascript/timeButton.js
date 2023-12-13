95fe5c1da540e1d7e1cd88028cf3fc97dad725e5
Faraday <115096700+Faraday22@users.noreply.github.com>
2023-12-13 19:49:28 +0000

    Trying to fix find period function

--- more-javascript/timeButton.js
+++ more-javascript/timeButton.js
@@ -1,6 +1,5 @@
 // import { halfDayPeriods } from "halfDayScript"; 
-
-var timeLeft = null;
+var timeLeft;
 var startTime = 8.47;
 var date = new Date();
 /*
@@ -8,10 +7,7 @@ var date = new Date();
 * echo's that the button is useless due to sat/sun day
 */
 var currentTime = date.getHours() + date.getMinutes() / 100;
-
 //var IsWeekend = date.getDay;
-
-
 var endOfAdvisory = 9.11;
 var endOfSecond = 9.57;
 var endOfThird = 10.48;
@@ -21,132 +17,121 @@ var endOfLunch = 13.05;
 var endOfSixth = 13.56;
 var endOfSeventh = 14.47;
 var endOfEigth = 15.40;
-
-
+var endOfSchool = 15.40;
 var schoolActive = null;
 // is school active / has it started
-if (currentTime <= 8.47){
-    schoolActive = schoolActive === null ? false : schoolActive; 
-} else if (currentTime >= startTime){
-    schoolActive = schoolActive === null ? true : schoolActive; 
-} else{
-  console.log("How tf was this printed??");
-} 
-
-function isAdvistory(){
-    if (currentTime >= startTime && currentTime > endOfAdvisory){
-      alert("Your in advisory");
-      timeLeft = 9.11 - currentTime;
-      alert("There is: " + timeLeft + " time left");
-      return;
+if (currentTime >= startTime && currentTime < endOfSchool) {
+    schoolActive = schoolActive === null ? true : schoolActive;
+}
+else if (currentTime > endOfSchool) {
+    schoolActive = schoolActive === null ? false : schoolActive;
+}
+else {
+    console.log("How tf was this printed??");
+}
+function isAdvistory() {
+    if (currentTime > startTime && currentTime > endOfAdvisory) {
+        alert("Your in advisory");
+        timeLeft = 9.11 - currentTime;
+        alert("There is: " + timeLeft + " time left");
+        return;
     }
 }
-
-function isSecond(){
-  if (currentTime > endOfAdvisory && currentTime > endOfSecond){
-    alert("Your in second period");
-    timeLeft = endOfSecond - currentTime;
-    alert("There is: " + timeLeft + " time left");
-    return;
-  }
-}
-
-function isThird(){
-  if (currentTime > endOfSecond && currentTime > endOfThird){
-    alert("Your in third period");
-    timeLeft = endOfThird - currentTime;
-    alert("There is: " + timeLeft + " time left");
-    return;
-  }
-}
-
-function isFourth(){
-  if (currentTime > endOfThird && currentTime > endOfFourth){
-    alert("Your in fourth period");
-    timeLeft = endOfFourth - currentTime;
-    alert("There is: " + timeLeft + " time left");
-    return;
-  } 
-}
-
-function isFifth(){
-  if (currentTime > endOfFourth && currentTime > endOfFifth){
-    alert("Your in fifth period");
-    timeLeft = endOfFifth - currentTime;
-    alert("There is: " + timeLeft + " time left");
-    return;
-  } 
-}
-
-function isLunch(){
-  if (currentTime > endOfFifth && currentTime > endOfLunch){
-
-    const easteregg = Math.floor(Math.random() * 100 ) + 1;
-
-    if (easteregg == 52){
-      alert("Your in munchy munch time");
-    } else{
-    alert("Your in  period");
+function isSecond() {
+    if (currentTime > endOfAdvisory && currentTime > endOfSecond) {
+        alert("Your in second period");
+        timeLeft = endOfSecond - currentTime;
+        alert("There is: " + timeLeft + " time left");
+        return;
     }
-    timeLeft = endOfSixth - currentTime;
-    alert("There is: " + timeLeft + " time left");
-    return;
-  }
 }
-
-function isSixth(){
-  if (currentTime > endOfLunch && currentTime > endOfSixth){
-    alert("Your in sixth period");
-    timeLeft = endOfSixth - currentTime;
-    alert("There is: " + timeLeft + " time left");
-    return;
-  } 
+function isThird() {
+    if (currentTime > endOfSecond && currentTime > endOfThird) {
+        alert("Your in third period");
+        timeLeft = endOfThird - currentTime;
+        alert("There is: " + timeLeft + " time left");
+        return;
+    }
 }
-
-function isSeventh(){
-  if (currentTime > endOfSixth && currentTime > endOfSeventh){
-    alert("Your in seventh period");
-    timeLeft = endOfSeventh - currentTime;
-    alert("There is: " + timeLeft + " time left");
-    return;
-  } 
+function isFourth() {
+    if (currentTime > endOfThird && currentTime > endOfFourth) {
+        alert("Your in fourth period");
+        timeLeft = endOfFourth - currentTime;
+        alert("There is: " + timeLeft + " time left");
+        return;
+    }
 }
-
-function isEigth(){
-  if (currentTime > endOfSeventh && currentTime > endOfEigth){
-    alert("Your in eight period");
-    timeLeft = endOfEigth - currentTime;
-    alert("There is: " + timeLeft + " time left");
-    return;
-  } 
+function isFifth() {
+    if (currentTime > endOfFourth && currentTime > endOfFifth) {
+        alert("Your in fifth period");
+        timeLeft = endOfFifth - currentTime;
+        alert("There is: " + timeLeft + " time left");
+        return;
+    }
 }
-
-function isSchoolOver(){
-  if (currentTime >= 15.40){
-    alert("Schools has ended");
-    schoolActive = false;
-    return;
-  }
+function isLunch() {
+    if (currentTime > endOfFifth && currentTime > endOfLunch) {
+        const easteregg = Math.floor(Math.random() * 100) + 1;
+        if (easteregg == 52) {
+            alert("Your in munchy munch time");
+        }
+        else {
+            alert("Your in  period");
+        }
+        timeLeft = endOfSixth - currentTime;
+        alert("There is: " + timeLeft + " time left");
+        return;
+    }
 }
-
-function findPeriod(){
-  isAdvistory();
-  isSecond();
-  isThird();
-  isFourth();
-  isFifth();
-  isLunch();
-  isSixth();
-  isSeventh();
-  isEigth();
-  isSchoolOver();
+function isSixth() {
+    if (currentTime > endOfLunch && currentTime > endOfSixth) {
+        alert("Your in sixth period");
+        timeLeft = endOfSixth - currentTime;
+        alert("There is: " + timeLeft + " time left");
+        return;
+    }
 }
-
-
-function schoolTimeCalc(){
-  if (schoolActive === true){
-    findPeriod();
-  } else{
-    alert("Your not even in school");
-  }
+function isSeventh() {
+    if (currentTime > endOfSixth && currentTime > endOfSeventh) {
+        alert("Your in seventh period");
+        timeLeft = endOfSeventh - currentTime;
+        alert("There is: " + timeLeft + " time left");
+        return;
+    }
 }
+function isEigth() {
+    if (currentTime > endOfSeventh && currentTime > endOfEigth) {
+        alert("Your in eight period");
+        timeLeft = endOfEigth - currentTime;
+        alert("There is: " + timeLeft + " time left");
+        return;
+    }
+}
+function isSchoolOver() {
+    if (currentTime >= 15.40) {
+        alert("Schools has ended");
+        schoolActive = false;
+        return;
+    }
+}
+function findPeriod() {
+    isAdvistory();
+    isSecond();
+    isThird();
+    isFourth();
+    isFifth();
+    isLunch();
+    isSixth();
+    isSeventh();
+    isEigth();
+    isSchoolOver();
+}
+function schoolTimeCalc() {
+    if (schoolActive === true) {
+        findPeriod();
+    }
+    else {
+        alert("Your not even in school");
+    }
+}
+schoolTimeCalc();
