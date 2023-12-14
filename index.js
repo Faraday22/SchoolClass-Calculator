7aac89dc7d0ed8dedf114bb9ad43a5f9a032393c
Faraday <115096700+Faraday22@users.noreply.github.com>
2023-12-14 14:45:56 -0600

    Added warning msg to console in index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import { checkTime } from "./more-javascript/countDown.mjs";
 
+var WarningMSG = `
+  Never run code in here
+  unless you understand
+  what your doing & what
+  it does!
+`;
+
+
+console.log(WarningMSG);
+
 checkTime(); 
